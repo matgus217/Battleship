839385c5a50dcd61e592b3b83a05a1842524aac0
matgus217 <>
2023-11-21 09:38:31 +0000

    Add a converter for letters and numbers

--- run.py
+++ run.py
@@ -4,3 +4,14 @@ HIDDEN_BOARD = [[" "] * 8 for x in range (8)]
 #Hits and misses
 GUESS_BOARD = [[" "] * 8 for in range (8)]
 
+
+letter_to_numbers = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+
+
+def board():
+    pass
+
+
+
+def print_board(board):
+    print(" A B C D E F G H ")
