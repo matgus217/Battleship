c298de8e86de389121543f13740db59016d34d63
matgus217 <>
2023-11-21 08:53:38 +0000

    Create borads for ships,hits and misses

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Holdning ships
+HIDDEN_BOARD = [[" "] * 8 for x in range (8)]
+
+#Hits and misses
+GUESS_BOARD = [[" "] * 8 for in range (8)]
+
