4bd8d073e60199cc09074b3d582c37bb17cf202c
matgus217 <>
2023-11-21 09:49:21 +0000

    Add functions, create ships, ship location and ships hit

--- run.py
+++ run.py
@@ -4,12 +4,32 @@ HIDDEN_BOARD = [[" "] * 8 for x in range (8)]
 #Hits and misses
 GUESS_BOARD = [[" "] * 8 for in range (8)]
 
-
+#Converter letter to numbers
 letter_to_numbers = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 
 
+#Functions
+
 def board():
     pass
+
+
+
+def create_ships():
+    pass
+
+
+def get_ship_location():
+    pass
+    
+def count_hit_ships():
+    pass
+
+create_ships()
+turns= 10
+#while turns > 0:
+
+
 
 
 
